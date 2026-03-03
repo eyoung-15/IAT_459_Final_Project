93553262ea0ca2486739db161f47734808a73e43
Emily Youngward <eyoungwa@gmail.com>
2026-03-03 15:12:33 -0800

    mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,42 @@
-// server.js
-const express = require('express');
-const cors = require('cors'); // Import the CORS package
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const plantRoutes = require("./routes/plants");
+
 const app = express();
 const PORT = 5000;
 
-// Middleware to parse JSON 
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
-// Enable CORS: Allow requests specifically from your React Frontend
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+// database connection
+const uri = process.env.MONGO_URI;
 
-// A test route
-app.get('/api/hello', (req, res) => {
-  res.json({ message: "Hello from the Node backend!" });
-});
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function connectDB() {
+  try {
+    await mongoose.connect(uri, clientOptions);
+
+    // optional: the "Ping" command just confirms everything is working
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("✅ Pinged the db. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error("❌ Connection failed:", err);
+    // If the DB is down, we might want to stop the server
+    // process.exit(1);
+  }
+}
+
+// execute the connection function
+connectDB();
+
+// routes
+app.use("/api/plants", plantRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
