f5d92d4439112cf77d9cc811731edc3c7a111d93
joshchin8880 <151962750+joshchin8880@users.noreply.github.com>
2026-03-10 13:35:43 -0700

    Update Dashboard.js

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
 import "./HeritageHub.css";
 
