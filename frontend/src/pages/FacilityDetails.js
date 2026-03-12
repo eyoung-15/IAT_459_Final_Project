f8d65ff021450ef6f6271fe148f95ce890f254bb
Emily Youngward <eyoungwa@gmail.com>
2026-03-11 18:33:36 -0700

    small fix

--- frontend/src/pages/FacilityDetails.js
+++ frontend/src/pages/FacilityDetails.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "./context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 import { useParams, Link } from "react-router-dom";
 
 function FacilityDetails() {
