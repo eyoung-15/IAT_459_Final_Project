1890eb5066a404acbb48d73b9978f26c97218925
joshchin8880 <151962750+joshchin8880@users.noreply.github.com>
2026-03-20 22:15:35 -0700

    credited png icon image source

--- frontend/src/pages/Map.js
+++ frontend/src/pages/Map.js
@@ -10,6 +10,8 @@ import "../css/App.css";
 // https://ujjwaltiwari2.medium.com/a-guide-to-using-openstreetmap-with-react-70932389b8b1
 // https://medevel.com/react-and-leaflet-js-tutorial/
 
+// Marker PNG by Vecteezy.com https://www.vecteezy.com/png/17178337-location-map-marker-icon-symbol-on-transparent-background
+
 const markerIcon = new L.Icon({
   iconUrl: require("../../src/images/placeholderMarker.png"),
   iconSize: [20, 20],
