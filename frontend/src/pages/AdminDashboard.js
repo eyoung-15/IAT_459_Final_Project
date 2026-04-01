39bd0dcc7a7795a7fdf138b6442f9a9fab17a302
joshchin8880 <151962750+joshchin8880@users.noreply.github.com>
2026-04-01 16:07:30 -0700

    added nav bar

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -3,7 +3,7 @@ import { AuthContext } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 
 function AdminDashboard() {
-  const { token, user } = useContext(AuthContext);
+  const { token, user, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
   //   Backup security - redirect user out if they aren't admin
@@ -14,8 +14,40 @@ function AdminDashboard() {
   }, [user, navigate]);
 
   return (
-    <div className="page-container">
-      <p>adminDashboard</p>
+    <div className="heritage-hub">
+      <nav className="navbar">
+        <div className="nav-container">
+          <div className="nav-left">
+            <Link to="/" className="logo">
+              Heritage<span>Hub</span>
+            </Link>
+            <div className="nav-links">
+              <Link to="/" className="nav-link active">
+                Explore
+              </Link>
+              <Link to="/Map" className="nav-link">
+                Map View
+              </Link>
+              <Link to="/" className="nav-link">
+                Curated Lists
+              </Link>
+              <Link to="/dashboard" className="nav-link">
+                Manage
+              </Link>
+            </div>
+          </div>
+
+          <div className="nav-right">
+            {user && (
+              <span className="user-greeting">Welcome, {user.username}!</span>
+            )}
+            <button onClick={logout} className="logout-btn">
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+      <p>Admin Panel..</p>
     </div>
   );
 }
