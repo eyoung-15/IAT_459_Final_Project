e2362e19cd5aef3e8b9a9ff2c13da6283368b933
Emily Youngward <eyoungwa@gmail.com>
2026-04-03 16:49:27 -0700

    fixes

--- backend/routes/facility.js
+++ backend/routes/facility.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Facility = require("../models/Facility");
 const Review = require("../models/Reviews");
+const User = require("../models/User");
 const verifyToken = require("../middleware/auth");
 
-// Get my own added facilities
+//Get my own added facilities
 router.get("/my-facilities", verifyToken, async (req, res) => {
   try {
     // filter database search by owner ID
@@ -18,26 +19,75 @@ router.get("/my-facilities", verifyToken, async (req, res) => {
 // GET ALL ROUTE
 router.get("/", async (req, res) => {
   try {
-    // .sort({_id: -1}) reverses the id's to ensure that the newest id's are displayed first
-    const facility = await Facility.find().sort({ _id: -1 }).limit(100);
+    const {
+      Category,
+      Province,
+      City,
+      searchTerm,
+      page = 1,
+      limit = 20,
+    } = req.query;
 
-    const facilitiesWithRatings = await Promise.all(
-      facility.map(async (facility) => {
-        const reviews = await Review.find({ facility: facility._id });
+    const pageNum = parseInt(page);
+    const limitNum = parseInt(limit);
+    const skip = (pageNum - 1) * limitNum;
 
-        const avgRating =
-          reviews.length > 0
-            ? reviews.reduce((sum, r) => sum + r.rating, 0) / reviews.length
-            : 0;
+    let matchStage = {};
 
-        return {
-          ...facility.toObject(),
-          avgRating: Number(avgRating.toFixed(1)),
-          reviewCount: reviews.length,
-        };
-      }),
-    );
-    res.json(facilitiesWithRatings);
+    if (Category) matchStage.Category = Category;
+    if (Province) matchStage.Province = Province;
+    if (City) matchStage.City = City;
+
+    if (searchTerm) {
+      matchStage.Name = { $regex: searchTerm, $options: "i"};
+    }
+
+    const facility = await Facility.aggregate([
+      { $match: matchStage },
+
+      {
+        $lookup: {
+          from: "reviews",
+          localField: "_id",
+          foreignField: "facility",
+          as: "reviews",
+        },
+      },
+      
+      //calculate rating + count
+      {
+        $addFields: {
+          avgRating: { $avg: "$reviews.rating" },
+          reviewCount: { $size: "$reviews"},
+        },
+
+      },
+
+      {
+        $project: {
+          reviews: 0,
+        },
+      },
+
+      //sort newest first
+      { $sort: {_id: -1}},
+
+      //pagination
+      {$skip: skip},
+      {$limit: limitNum},
+
+
+    ]);
+    
+    //get total for UI
+    const total = await Facility.countDocuments(matchStage);
+
+    res.json({
+      data: facility,
+      currentPage: pageNum,
+      totalPages: Math.ceil(total / limitNum),
+      totalItems: total,
+    });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -96,7 +146,7 @@ router.post("/", verifyToken, async (req, res) => {
 // DELETE ROUTE
 router.delete("/:id", verifyToken, async (req, res) => {
   try {
-    const facility = await Facility.findByIdAndDelete(req.params.id);
+    const facility = await Facility.findById(req.params.id);
 
     // check if facility exists
     if (!facility) {
@@ -116,6 +166,16 @@ router.delete("/:id", verifyToken, async (req, res) => {
     }
 
     await Facility.findByIdAndDelete(req.params.id);
+
+    await User.updateMany(
+      {},
+      {
+        $pull: {
+          bucketList: req.params.id,
+          visited: { facility: req.params.id },
+        },
+      }
+    );
     res.json({ message: "Facility successfully deleted" });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
