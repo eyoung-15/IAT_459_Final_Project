78a779a3f4d69e325ff34bf3c23a073af660d03d
joshchin8880 <151962750+joshchin8880@users.noreply.github.com>
2026-04-02 20:08:39 -0700

    added post/delete routes to support admin functionalities

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,123 +5,161 @@ const verifyToken = require("../middleware/auth");
 
 //Add to bucket list
 router.post("/bucket/:facilityId", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
 
-    try{
-        const user = await User.findById(req.user.id);
-        
-        if(!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-        
-        //prevent duplicates
-        const exists = user.bucketList.some(
-            id => id.toString() === req.params.facilityId
-        );
-        
-        if (!exists) {
-            user.bucketList.push(req.params.facilityId);
-            await user.save();
-        }
-        
-        res.json({message: "Added to Bucket List", bucketList: user.bucketList});
-    
-    }catch(err){
-        res.status(500).json({message: err.message});
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    //prevent duplicates
+    const exists = user.bucketList.some(
+      (id) => id.toString() === req.params.facilityId,
+    );
+
+    if (!exists) {
+      user.bucketList.push(req.params.facilityId);
+      await user.save();
+    }
+
+    res.json({ message: "Added to Bucket List", bucketList: user.bucketList });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 //Remove from bucket list
 router.delete("/bucket/:facilityId", verifyToken, async (req, res) => {
-    try{
-        const user = await User.findById(req.user.id);
+  try {
+    const user = await User.findById(req.user.id);
 
-        if (!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-
-        user.bucketList = user.bucketList.filter(
-            id => id.toString() !== req.params.facilityId
-        );
-
-        await user.save();
-        
-        res.json({message: "Removed from Bucket List", bucketList: user.bucketList});
-
-    } catch (err) {
-        res.status(500).json({message: err.message});
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    user.bucketList = user.bucketList.filter(
+      (id) => id.toString() !== req.params.facilityId,
+    );
+
+    await user.save();
+
+    res.json({
+      message: "Removed from Bucket List",
+      bucketList: user.bucketList,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 // Add visited (with image)
 router.post("/visited/:facilityId", verifyToken, async (req, res) => {
-    try{
-        const {image} = req.body;
-        const user = await User.findById(req.user.id);
+  try {
+    const { image } = req.body;
+    const user = await User.findById(req.user.id);
 
-        if (!user){
-            return res.status(404).json({message: "User not found"});
-        }
-        
-        const exists = user.visited.find(
-            v => v.facility.toString() === req.params.facilityId
-        );
-        
-        if (!exists){
-            user.visited.push({
-                facility: req.params.facilityId,
-                image,
-                visitedAt: new Date()
-        });
-        await user.save();
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
-    res.json({message: "Marked as Visited", visited: user.visited});
+    const exists = user.visited.find(
+      (v) => v.facility.toString() === req.params.facilityId,
+    );
 
-} catch (err){
-    res.status(500).json({message: err.message});
-}
+    if (!exists) {
+      user.visited.push({
+        facility: req.params.facilityId,
+        image,
+        visitedAt: new Date(),
+      });
+      await user.save();
+    }
 
+    res.json({ message: "Marked as Visited", visited: user.visited });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 //remove from visited
-router.delete("/visited/:facilityId", verifyToken, async(req, res) => {
-    try{
-        const user = await User.findById(req.user.id);
+router.delete("/visited/:facilityId", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
 
-        user.visited = user.visited.filter(
-            v => v.facility.toString() !== req.params.facilityId
-        );
+    user.visited = user.visited.filter(
+      (v) => v.facility.toString() !== req.params.facilityId,
+    );
 
-        await user.save();
+    await user.save();
 
-        res.json({message: "Removed from Visited", visited: user.visited});
-    } catch (err) {
-        res.status(500).json({message: err.message});
-    }
+    res.json({ message: "Removed from Visited", visited: user.visited });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 //get user data and stats
 router.get("/me", verifyToken, async (req, res) => {
-    try{
-        const user = await User.findById(req.user.id)
-        .populate("bucketList")
-        .populate("visited.facility");
+  try {
+    const user = await User.findById(req.user.id)
+      .populate("bucketList")
+      .populate("visited.facility");
 
-        if(!user) {
-            return res.status(404).json({message: "User not found"});
-        }
-        
-        const stats = {
-            visitedCount: user.visited.length,
-            bucketCount: user.bucketList.length
-        };
-
-        res.json({user, stats});
-
-    } catch (err) {
-        res.status(500).json({message: err.message});
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
 
+    const stats = {
+      visitedCount: user.visited.length,
+      bucketCount: user.bucketList.length,
+    };
+
+    res.json({ user, stats });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// get all users (for admin dashboard)
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find().sort({ _id: -1 });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// DELETE ROUTE (for admins)
+router.delete("/:id", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    // check if user exists
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // authorization check: assess if their admin
+    const admin = req.user.role === "admin";
+
+    // If not a match, reject deletion
+    if (!admin) {
+      return res.status(403).json({
+        message: "Forbidden: You do not have permission to delete this user.",
+      });
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User successfully deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
 });
 
 module.exports = router;
