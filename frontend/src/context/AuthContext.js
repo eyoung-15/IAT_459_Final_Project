758819b96d176e5a7cbd20e2c73926777cf1f4b4
joshchin8880 <151962750+joshchin8880@users.noreply.github.com>
2026-04-06 14:02:16 -0700

    added function to logout upon token expiry

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -8,6 +8,7 @@ export function AuthProvider({ children }) {
   //   initialize token in localStorage
   const [token, setToken] = useState(localStorage.getItem("token") || null);
   const [user, setUser] = useState(null);
+  const [timeoutMsg, setTimeoutMsg] = useState("");
 
   // run when token changes (login/logout/upon load)
   useEffect(() => {
@@ -26,10 +27,55 @@ export function AuthProvider({ children }) {
     }
   }, [token]);
 
+  // Set token timer to logout user when token expires
+  useEffect(() => {
+    if (!token) return;
+
+    try {
+      const decoded = jwtDecode(token);
+
+      // change expiration time to miliseconds to match date.now
+      const expiration = decoded.exp * 1000;
+      const timeout = expiration - Date.now();
+      // Logout when timer is done
+      if (timeout <= 0) {
+        logout();
+        return;
+      }
+      // Set warning time of 30 seconds before timeout
+      const warning = timeout - 30000;
+      let warningTimer = null;
+      if (warning > 0) {
+        warningTimer = setTimeout(() => {
+          setTimeoutMsg(
+            "Your session is about to expire. You will be logged out in 30 seconds.",
+          );
+        }, warning);
+      } else {
+        setTimeoutMsg("Your session is about to expire.");
+      }
+
+      const timer = setTimeout(() => {
+        logout();
+      }, timeout);
+
+      // clear once done
+      return () => {
+        clearTimeout(timer);
+        if (warningTimer) clearTimeout(warningTimer);
+      };
+    } catch (err) {
+      console.error("Error setting logout timer:", err);
+      logout();
+    }
+  }, [token]);
+
   //handle login and set token
   function login(newToken) {
     localStorage.setItem("token", newToken);
     setToken(newToken);
+    // Ensure timeoutMsg is empty upon login
+    setTimeoutMsg("");
   }
 
   //handle logout and reset
@@ -37,11 +83,13 @@ export function AuthProvider({ children }) {
     localStorage.removeItem("token");
     setToken(null);
     setUser(null);
+    // Ensure timeoutMsg is empty if user is not logged in
+    setTimeoutMsg("");
   }
 
   return (
     // Return the token, user, and the functions (login/out)
-    <AuthContext.Provider value={{ token, user, login, logout }}>
+    <AuthContext.Provider value={{ token, user, login, logout, timeoutMsg }}>
       {children}
     </AuthContext.Provider>
   );
